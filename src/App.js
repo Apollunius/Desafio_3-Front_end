3b4b3c2cf1e5cc80e470ac727a7916860874c1d8
Thauan Costa <thauan.dcosta@gmail.com>
2020-10-26 15:57:00 -0300

    Documentação: Novo Projeto

---
+++ src/App.js
@@ -0,0 +1,338 @@
+import React from "react";
+import "./App.css";
+import { arrow_left } from "./assets/index";
+import { arrow_right } from "./assets/index";
+import { arrow_up } from "./assets/index";
+import { arrow_down } from "./assets/index";
+import { pen } from "./assets/index";
+import { sort } from "./assets/index";
+import { check } from "./assets/index";
+
+const solicitarRodada = (setDadosRodada, setNovaRodada, rodada) => {
+  fetch(`http://localhost:8081/jogos/${rodada}`)
+    .then((res) => res.json())
+    .then((dados) => {
+      setDadosRodada(dados.dados);
+      setNovaRodada(dados.dados[0].rodada);
+    });
+};
+const solicitarClassificacao = (setClassificando) => {
+  fetch("http://localhost:8081/classificacao")
+    .then((res) => res.json())
+    .then((dados) => {
+      const newPosition = dados.dados.map((l, p) => {
+        l.posicao = p + 1;
+        return l;
+      });
+      setClassificando(newPosition);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export default function App() {
+  const editarRodada = (token, id, golsCasa, golsVisitante) => {
+    return fetch("http://localhost:8081/jogos", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token && `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        id: id,
+        golsCasa: golsCasa,
+        golsVisitante: golsVisitante,
+      }),
+    }).catch((err) => {
+      console.error(err);
+    });
+  };
+
+  function TabelaClassificacao() {
+    const legenda = {
+      posicao: "Posição",
+      time: "Time",
+      pontos: "PTS",
+      empates: "E",
+      vitorias: "V",
+      derrotas: "D",
+      golsFeitos: "GF",
+      golsSofridos: "GS",
+      saldoGols: "SG",
+    };
+
+    const colunas = [
+      "posicao",
+      "time",
+      "pontos",
+      "empates",
+      "vitorias",
+      "derrotas",
+      "golsFeitos",
+      "golsSofridos",
+      "saldoGols",
+    ];
+
+    const [colunaOrdenada, setColunaOrdenada] = React.useState("pontos");
+    const [ordem, setOrdem] = React.useState("descendente");
+
+    const dadosAscendentes = cassificando.sort((t1, t2) => {
+      if (
+        typeof t1[colunaOrdenada] === "number" &&
+        typeof t2[colunaOrdenada] === "number"
+      ) {
+        return (
+          parseInt(t2[colunaOrdenada], 10) - parseInt(t1[colunaOrdenada], 10)
+        );
+      } else {
+        return t1[colunaOrdenada].localeCompare(t2[colunaOrdenada]);
+      }
+    });
+    const dadosOrdenados =
+      ordem === "descendente" ? dadosAscendentes : dadosAscendentes.reverse();
+    return (
+      <div className="tabela">
+        <table className="cor">
+          <tr className="headerTabela">
+            {colunas.map((coluna) => (
+              <th>
+                {legenda[coluna]}{" "}
+                <img
+                  alt=""
+                  src={
+                    colunaOrdenada !== coluna
+                      ? sort
+                      : ordem === "descendente"
+                      ? arrow_down
+                      : arrow_up
+                  }
+                  onClick={() => {
+                    if (colunaOrdenada === coluna) {
+                      setOrdem((ordem) =>
+                        ordem === "descendente" ? "ascendente" : "descendente"
+                      );
+                    } else {
+                      setColunaOrdenada(coluna);
+                      setOrdem("descendente");
+                    }
+                  }}
+                />
+              </th>
+            ))}
+          </tr>
+
+          {dadosOrdenados.map((time, i) => (
+            <tr className="bodyTabela">
+              {colunas.map((coluna) =>
+                coluna === "saldoGols" ? (
+                  <td>
+                    {dadosOrdenados[i].golsFeitos -
+                      dadosOrdenados[i].golsSofridos}
+                  </td>
+                ) : (
+                  <td>{time[coluna]}</td>
+                )
+              )}
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
+  function Rodadas(props) {
+    const [editando, setEditando] = React.useState(false);
+    return (
+      <div>
+        <table>
+          <thead className="escolherRodada">
+            <tr>
+              <img
+                src={arrow_left}
+                alt=""
+                onClick={() => {
+                  if (rodada > 1) {
+                    setRodada(rodada - 1);
+                  }
+                }}
+              />
+            </tr>
+            <tr>
+              <div className="titleRodada">{rodada}ª Rodada </div>
+            </tr>
+            <tr>
+              <img
+                className="right"
+                src={arrow_right}
+                alt="arrow_right"
+                onClick={() => {
+                  if (rodada < 38) {
+                    setRodada(rodada + 1);
+                  }
+                }}
+              />
+            </tr>
+          </thead>
+          <div className="tabelaRodada">
+            <tbody>
+              {dadosRodada.map((x) => {
+                return (
+                  <tr className="testando">
+                    <td className="casa">{x.time_casa}</td>
+                    <div>
+                      {id !== x.id ? (
+                        <td className="score">{x.gols_casa}</td>
+                      ) : (
+                        <input
+						  type="number"
+						  className="inputGol"
+                          value={golsCasa}
+                          onChange={(event) => setGolsCasa(event.target.value)}
+                        />
+                      )}
+                      {rodada === null ? null : <td>&times;</td>}
+                      {id !== x.id ? (
+                        <td className="score">{x.gols_visitante}</td>
+                      ) : (
+                        <input
+						  type="number"
+						  className="inputGol"
+                          value={golsVisitante}
+                          onChange={(event) =>
+                            setGolsVisitante(event.target.value)
+                          }
+                        />
+                      )}
+                    </div>
+                    <td className="visitante">{x.time_visitante} </td>
+                    {token && (
+                      <img
+                        src={pen}
+                        alt="editar"
+                        onClick={() => {
+                          if (id === x.id) {
+                            setId(null);
+
+                            editarRodada(
+                              token,
+                              id,
+                              golsCasa,
+                              golsVisitante
+                            ).then(() => {
+                              solicitarClassificacao(setClassificando);
+                              solicitarRodada(rodada);
+                            });
+                          } else {
+                            setId(x.id);
+                            setGolsCasa(x.gols_casa);
+                            setGolsVisitante(x.gols_visitante);
+                          }
+                        }}
+                      />
+                    )}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </div>
+        </table>
+      </div>
+    );
+  }
+  function fazerRequisicaoComBody(url, metodo, conteudo, token) {
+    return fetch(url, {
+      method: metodo,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token && `Bearer ${token}`,
+      },
+      body: JSON.stringify(conteudo),
+    });
+  }
+  function Login(props) {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const { token, setToken } = props;
+
+    return (
+      <div>
+        {token ? (
+          <button onClick={() => setToken(null)} className="btnLogin">
+            {" "}
+            Deslogar{" "}
+          </button>
+        ) : (
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              fazerRequisicaoComBody("http://localhost:8081/auth", "POST", {
+                email,
+                password,
+              })
+                .then((res) => res.json())
+                .then((respostaJson) => {
+                  const novoToken = respostaJson.dados.token;
+                  setToken(novoToken);
+                  setEmail("");
+                  setPassword("");
+                });
+            }}
+          >
+            <label>
+              {"Email"}{" "}
+              <input
+                type="email"
+                className="iptLogin"
+                value={email}
+                onInput={(event) => setEmail(event.target.value)}
+              />
+            </label>
+            <label>
+              {"Senha"}{" "}
+              <input
+                className="iptLogin"
+                type="password"
+                value={password}
+                onInput={(event) => setPassword(event.target.value)}
+              />
+            </label>
+
+            <button className="btnLogin">Logar</button>
+          </form>
+        )}
+      </div>
+    );
+  }
+  const [rodada, setRodada] = React.useState(1);
+  const [dadosRodada, setDadosRodada] = React.useState([]);
+  const [novaRodada, setNovaRodada] = React.useState(null);
+  const [token, setToken] = React.useState(null);
+  const [cassificando, setClassificando] = React.useState([]);
+  const [id, setId] = React.useState(null);
+  const [golsCasa, setGolsCasa] = React.useState();
+  const [golsVisitante, setGolsVisitante] = React.useState();
+
+  React.useEffect(() => {
+    solicitarClassificacao(setClassificando);
+  }, []);
+  React.useEffect(() => {
+    solicitarRodada(setDadosRodada, setNovaRodada, rodada);
+  }, [rodada]);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="center">
+          <h1>Brasileirão</h1>
+          <Login token={token} setToken={setToken} />
+        </div>
+      </div>
+      <div className="content">
+        <div className="center2">
+          <Rodadas />
+          <TabelaClassificacao />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import arrow_left from "./arrow_left.svg";
+import arrow_right from "./arrow_right.svg";
+import arrow_up from "./arrow_up.svg";
+import arrow_down from "./arrow_down.svg";
+import check from "./check.svg";
+import pen from "./pen.svg";
+import sort from "./sort.svg";
+
+export { arrow_left, arrow_right, arrow_up, arrow_down, check, pen, sort };
